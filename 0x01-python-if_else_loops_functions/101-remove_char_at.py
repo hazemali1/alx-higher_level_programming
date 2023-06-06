34252793bbda6420460c671a2a1f820aa79103b6
hazemali1 <hazemali1343@gmail.com>
2023-06-06 15:40:31 -0700

    remove at pos

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
+    Str = ''
     for w in range(len(str)):
         if w != n:
-            print('{}'.format(str[w]), end='')
+            Str += str[w]
+        return Str
