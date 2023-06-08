18f4e666a69baa71431fa154045462a590e537e9
hazemali1 <hazemali1343@gmail.com>
2023-06-08 16:13:05 -0700

    my cal

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import calculator_1 as cal
+    import sys
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    elif sys.argv[2] == "+":
+        print("{} + {} = {}".format(sys.argv[1], sys.argv[3], cal.add(sys.argv[1], sys.argv[3])))
+    elif sys.argv[2] == "-":
+        print("{} - {} = {}".format(sys.argv[1], sys.argv[3], cal.sub(sys.argv[1], sys.argv[3])))
+    elif sys.argv[2] == "*":
+        print("{} * {} = {}".format(sys.argv[1], sys.argv[3], cal.mul(sys.argv[1], sys.argv[3])))
+    elif sys.argv[2] == "/":
+        print("{} / {} = {}".format(sys.argv[1], sys.argv[3], cal.div(sys.argv[1], sys.argv[3])))
+    else:
+        print(Unknown operator. Available operators: +, -, * and /)
+        exit(1)
