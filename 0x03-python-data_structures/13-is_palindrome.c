ecd4ec1e99b8a1c7640b174f4b59f9941342f311
hazemali1 <hazemali1343@gmail.com>
2023-06-11 14:33:38 -0700

    check

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * is_palindrome - Check
+ *
+ * @head: Parameter
+ *
+ * Return: 0, 1
+*/
+int is_palindrome(listint_t **head)
+{
+	printf("lol");
+	listint_t *s, *d, *f;
+
+	s = *head;
+	d = *head;
+	while (d->next != NULL)
+		d = d->next;
+	while (s != d)
+	{
+		if (s->n != d->n)
+			return (0);
+		f = *head;
+		while (f->next != d)
+			f = f->next;
+		s = s->next;
+		d = f;
+	}
+	return (1);
+}
