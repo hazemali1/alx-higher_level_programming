80be45ec901ba336cb8ecd8db573cf91193c65f0
hazemali1 <hazemali1343@gmail.com>
2023-06-14 17:43:44 -0700

    roman number

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    s = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    d = 0
+    for i in range(len(roman_string)):
+        if len(roman_string) == 1:
+            return s[roman_string[i]]
+        elif i == len(roman_string) - 1:
+            d += s[roman_string[i]]
+        elif s[roman_string[i]] >= s[roman_string[i + 1]]:
+            d += s[roman_string[i]]
+        elif s[roman_string[i]] < s[roman_string[i + 1]]:
+            d -= s[roman_string[i]]
+    return d
