a5619e2bf00360e3cbf812e3ce64656697ad644a
hazemali1 <hazemali1343@gmail.com>
2023-06-27 09:10:32 -0700

    safe list with int

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[s]), end="")
             q = q + 1
-        except Exception:
+        except ValueError:
             pass
     print('')
     return q
