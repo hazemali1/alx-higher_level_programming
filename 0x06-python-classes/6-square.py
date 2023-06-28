7a98025dff100f801bf3870733c9b6c1891b6ba7
hazemali1 <hazemali1343@gmail.com>
2023-06-28 11:10:35 -0700

    print _ with #

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -39,7 +39,8 @@ class Square:
     def my_print(self):
         if self.__size == 0:
             print('')
-            return
+        for n in range(self.__position[1]):
+            print('')
         for i in range(self.__size):
             for w in range(self.__position[0]):
                 print(' ', end="")
