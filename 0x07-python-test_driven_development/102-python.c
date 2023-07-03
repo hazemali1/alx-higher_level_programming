472673253d9c953ddeccabd00245a073d5c8711d
hazemali1 <hazemali1343@gmail.com>
2023-07-03 03:50:13 -0700

    python with c

---
+++ 0x07-python-test_driven_development/102-python.c
@@ -0,0 +1,23 @@
+#include <Python.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_python_string - Print string
+ * @p: Parameter
+*/
+void print_python_string(PyObject *p)
+{
+	printf("[.] string object info\n");
+	if (strcmp(p->ob_type->tp_name, "str") != 0)
+	{
+		printf("  [ERROR] Invalid String Object\n");
+		return;
+	}
+	if (PyUnicode_IS_ASCII(p))
+		printf("  type: compact ascii\n");
+	else
+		printf("  type: compact unicode object\n");
+	printf("  length: %d\n", strlen(p));
+	printf("  value: %s\n", p);
+}
