e1b0cc2d94a44f52c6dd3771842de2d16a279bf0
hazemali1 <hazemali1343@gmail.com>
2023-07-03 12:28:43 -0700

    queen

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""
+nqueens N
+"""
+
+
+import sys
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    if not sys.argv[1].isdigit():
+        print("Nshould be int")
+        sys.exit(1)
+    if int(sys.argv[1]) < 4:
+        print("N should be aleast 4")
+        sys.exit(1)
+    s = []
+    for i in range(int(sys.argv[1])):
+        s.append([])
+    d = 0
+    for j in s:
+        j.append(d)
+        j.append(d)
+        d += 1
+    print(s)
