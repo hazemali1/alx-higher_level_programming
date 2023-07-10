5a6c8ecb579e1d518c7f41926b0413a8ff66e170
hazemali1 <hazemali1343@gmail.com>
2023-07-10 04:30:53 -0700

    check

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+taking two element
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    check type of obj
+    """
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
