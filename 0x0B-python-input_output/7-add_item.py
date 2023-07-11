468ac87fb72fcc9d293b8ce7ede169cecb7be084
hazemali1 <hazemali1343@gmail.com>
2023-07-11 06:17:09 -0700

    load add save

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+define function
+"""
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
+import json
+import sys
+import os
+
+
+if path.isfile(add_item.json):
+    my_list = load_from_json_file(add_item.json)
+else:
+    my_list = []
+for s in sys.argv[1:]:
+    my_list.append(s)
+save_to_json_file(my_list, add_item.json)
