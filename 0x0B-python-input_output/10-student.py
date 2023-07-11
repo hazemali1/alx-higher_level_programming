0a3ecb562ecf9d51205a14f53e50e790847f3a96
hazemali1 <hazemali1343@gmail.com>
2023-07-11 06:46:39 -0700

    class

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+define class
+"""
+
+
+class Student:
+    """
+    Student classes
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        init function
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        class to json
+        """
+        if (type(attrs) == list and
+                all(type(e) == str for e in attrs)):
+            return {s: getattr(self, s) for s in attrs}
+        else:
+            return self.__dict__
