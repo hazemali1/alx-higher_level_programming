cad73bc62e9982752cd43e919d06828720eb9e1d
hazemali1 <hazemali1343@gmail.com>
2023-07-11 06:29:41 -0700

    load add save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,11 +2,11 @@
 """
 define function
 """
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
 import json
 import sys
 from os import path
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 if path.isfile("add_item.json"):
