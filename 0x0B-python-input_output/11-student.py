ccc70916a1e6f010373cb91acd02c405b7d39285
hazemali1 <hazemali1343@gmail.com>
2023-07-11 07:10:51 -0700

    update the value

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+define class
+"""
+
+
+class Student:
+    """
+    Student classes
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        init function
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        class to json
+        """
+        if (type(attrs) == list and
+                all(type(e) == str for e in attrs)):
+            return {s: getattr(self, s) for s in attrs if hasattr(self, s)}
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """
+        reload
+        """
+        for s, d in json.items():
+            setattr(self, s, d)
