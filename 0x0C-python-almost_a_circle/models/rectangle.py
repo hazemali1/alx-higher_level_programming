2ebef0e4db1e34cedf6dda9ae7f48048956110b2
hazemali1 <hazemali1343@gmail.com>
2023-07-17 13:09:26 -0700

    update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -2,7 +2,7 @@
 """
 define class
 """
-from base import Base
+from models.base import Base
 
 
 class Rectangle(Base):
@@ -10,6 +10,9 @@ class Rectangle(Base):
     Rectangle class
     """
     def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        define varibales
+        """
         self.width = width
         self.height = height
         self.x = x
@@ -18,10 +21,16 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """
+        width
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """
+        width
+        """
         if type(value) != int:
             raise TypeError("width must be an integer")
         if value <= 0:
@@ -30,10 +39,16 @@ class Rectangle(Base):
 
     @property
     def height(self):
+        """
+        height
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """
+        height
+        """
         if type(value) != int:
             raise TypeError("height must be an integer")
         if value <= 0:
@@ -42,10 +57,16 @@ class Rectangle(Base):
 
     @property
     def x(self):
+        """
+        x
+        """
         return self.__x
 
     @x.setter
     def x(self, value):
+        """
+        x
+        """
         if type(value) != int:
             raise TypeError("x must be an integer")
         if value < 0:
@@ -54,10 +75,16 @@ class Rectangle(Base):
 
     @property
     def y(self):
+        """
+        y
+        """
         return self.__y
 
     @y.setter
     def y(self, value):
+        """
+        y
+        """
         if type(value) != int:
             raise TypeError("y must be an integer")
         if value < 0:
@@ -65,9 +92,15 @@ class Rectangle(Base):
         self.__y = value
 
     def area(self):
+        """
+        area
+        """
         return self.__width * self.__height
 
     def display(self):
+        """
+        display
+        """
         for s in range(self.__y):
             print("")
         for i in range(self.__height):
@@ -78,12 +111,18 @@ class Rectangle(Base):
             print("")
 
     def __str__(self):
+        """
+        str function
+        """
         return ("[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
                                                         self.x, self.y,
                                                         self.width,
                                                         self.height))
 
     def update(self, *args, **kwargs):
+        """
+        update args
+        """
         if args and len(args) != 0:
             x = 1
             for a in args:
@@ -118,6 +157,9 @@ class Rectangle(Base):
                     self.y = value
 
     def to_dictionary(self):
+        """
+        to_dictionary
+        """
         return {
             "x": self.x,
             "y": self.y,
