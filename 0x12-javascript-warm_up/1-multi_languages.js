6043b7af58e42117e60eab3253e9d31d8c8347a1
hazemali1 <hazemali1343@gmail.com>
2023-09-11 14:56:47 -0700

    3

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const s = 'C is fun', d = 'Python is cool', q = 'JavaScript is amazing';
+const s = 'C is fun'; const d = 'Python is cool'; const q = 'JavaScript is amazing';
 console.log(s);
 console.log(d);
 console.log(q);
