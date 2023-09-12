e055a46b1577a23e99c94b89a3f8f8f07ee167ee
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-09-12 18:12:55 +0300

    print

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -10,7 +10,7 @@ class Rectangle {
   print () {
     for (let i = 0; i < this.height; i++) {
       for (let j = 0; j < this.width; j++) {
-        ProcessingInstruction.stdout.write('X');
+        Process.stdout.write('X');
       }
       console.log();
     }
