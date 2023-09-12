82c1df377191d6ecc811c88fa6cb8023a68f218a
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-09-12 22:18:15 +0300

    sort dict

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const dict = require('./101-data').dict;
+const newdict = {};
+for (const id in dict) {
+  if (dict.id in newdict) {
+    newdict[dict.id].push(id);
+  } else {
+    newdict[dict.id] = [id];
+  }
+}
+console.log(newdict);
