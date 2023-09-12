66f580a370900c8743f348a93f40c0bfb38281b4
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-09-12 03:11:00 +0300

    add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
 function add(a, b) {
-  return a + b;
+  return (a + b);
 }
 
-console.log(add(process.argv[2], process.argv[3]));
+if (process.argv.length === 4) {
+  console.log(add(process.argv[2], process.argv[3]));
+} else {
+  console.log(NaN);
+}
