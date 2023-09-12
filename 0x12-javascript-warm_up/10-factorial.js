682d6086184422df425945cad8f59a399de163db
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-09-12 03:18:59 +0300

    factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+function factorial(s) {
+  return s == 1? 1 : s * factorial(s - 1);
+}
+
+let d;
+if (process.argv.length === 2) {
+  d = factorial(1);
+} else {
+  d = factorial(process.argv[2]);
+}
+console.log(d);
