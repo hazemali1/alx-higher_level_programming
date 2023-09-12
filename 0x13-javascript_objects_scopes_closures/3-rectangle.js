e7b720c0a8976292b950a2879662eede487ddea6
hazemali1 <hazemali1343@gmail.com>
2023-09-12 08:15:13 -0700

    draw

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -10,7 +10,7 @@ class Rectangle {
   print () {
     for (let i = 0; i < this.height; i++) {
       for (let j = 0; j < this.width; j++) {
-        Process.stdout.write('X');
+        process.stdout.write('X');
       }
       console.log();
     }
