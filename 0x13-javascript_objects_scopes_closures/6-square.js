ebcac287e94920bea78c8fdd9d9b2975c44193b6
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-09-12 18:46:11 +0300

    c

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    for (let i = 0; i < this.size; i++) {
+      for (let j = 0; j < this.size; j++) {
+        if (c === undefined) {
+          process.stdout.write('X');
+        } else {
+          process.stdout.write(c);
+        }
+      }
+      console.log();
+    }
+  }
+}
+module.exports = Square;
