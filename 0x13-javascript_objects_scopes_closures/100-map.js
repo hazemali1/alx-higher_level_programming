aea481851c61913d445ebef549f39d3b0fe5a3d2
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-09-12 21:59:46 +0300

    map

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
-const list = require('./100-data');
+const list = require('./100-data').list;
 console.log(list);
 let d = -1;
-console.log(list.map(function (s) {
+const initiallist = list.map(function (s) {
   d++;
   return s * d;
-}));
+});
+console.log(initiallist);
