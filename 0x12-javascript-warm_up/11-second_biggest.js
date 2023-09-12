473987c0fa5b9dd303150cadf515a659f6963cd7
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-09-12 03:38:17 +0300

    second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,9 +2,9 @@
 if (process.argv.length === 2) {
   console.log(0);
 } else if (process.argv.length === 3) {
-  console.log(1);
+  console.log(0);
 } else {
-	let d;
+	let d = [];
   for (let i = 2; i < process.argv.length; i++) {
     d[i] = process.argv[i];
   }
