15e35e4cedaed85d177da8deb09f735ebf998570
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-09-15 18:38:50 +0300

    create datebase and user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- Create datebase and user
 CREATE DATEBASE IF NOT EXISTS 'hbtn_0d_2';
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
-IDENIFIED BY 'user_0d_2_pwd';
+IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
