2526ce84a148122bbdce710a787e9812e46c26c4
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-09-15 23:15:31 +0300

    tv show

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,3 +1,3 @@
 -- tv shows
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows, tv_show_genres
-ORDED BY tv_show.title and tv_show_genres.genre_id
+ORDED BY tv_show.title, tv_show_genres.genre_id;
