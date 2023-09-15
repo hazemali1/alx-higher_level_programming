77792fdc85afb90c96d7fe980b316745fe23cf9c
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-09-16 01:57:10 +0300

    raing

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,4 +1,5 @@
 -- rating
 SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating FROM tv_shows
 LEFT JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
+GROUP BY title
 ORDER BY rating DESC;
