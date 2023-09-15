d7a386d32ad00209b70588021e6eec3c440040f4
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-09-16 01:12:00 +0300

    genres

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,6 +1,6 @@
 -- dexter
 SELECT tv_genres.name FROM tv_genres
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id
+JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
 WHERE tv_shows.title = 'Dexter'
 ORDER BY name;
