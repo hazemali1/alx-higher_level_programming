2bd46047ee90da3167d1d5bbf1638214f7dd01ec
hazemali1 <hazemali1343@gmail.com>
2023-09-17 08:42:51 -0700

    join

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+get all states
+"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        database=argv[3]
+    )
+
+    mycursor = db.cursor()
+    mycursor.execute("SELECT * FROM states ORDER BY states.id")
+
+    for row in mycursor.fetchall():
+        print(row)
