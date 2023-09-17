482270ec9e7c0279e4c7620544d5333a10e928ae
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-09-17 14:41:18 +0300

    select all states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+get all states
+"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        database=argv[3]
+    )
+
+    mycursor = db.cursor()
+    s = "SELECT * FROM states WHERE LIKE %s ORDER BY states.id"
+    d = 'N%'
+    mycursor.execute(s, d)
+
+    for row in mycursor.fetchall():
+        print(row)
