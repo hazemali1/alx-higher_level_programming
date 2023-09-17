fa76e59193ce8860ab6ff8abf3c84d7fad315a83
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-09-17 12:00:20 +0300

    select all states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+get all states
+"""
+import MySQLdb
+from sys import argv
+
+db = MySQLdb.connect(
+	host="localhost",
+	port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        database=argv[3]
+)
+
+mycursor = db.cursor()
+mycursor.execute("SELECT * FROM states ORDER BY states.id")
+
+for row in mycursor.fatchall:
+	print(row)
