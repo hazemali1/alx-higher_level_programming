c148e010bf48b17838bf1f65405781d3395d0f16
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-09-17 23:24:26 +0300

    alchemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -12,10 +12,11 @@ if __name__ == "__main__":
     s = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
-    Base.metadata.create_all(s)
 
     Session = sessionmaker(bind=engine)
     d = Session()
+
+    Base.metadata.create_all(s)
 
     result = d.query(State).order_by(State.id).all()
 
