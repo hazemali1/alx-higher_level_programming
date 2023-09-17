061363f9e02de9be672925862e97ce577ee5e542
hazemali1 <hazemali1343@gmail.com>
2023-09-17 14:09:52 -0700

    id

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+class
+"""
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+
+if __name__ == "__main__":
+    s = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all(s)
+
+    Session = sessionmaker(bind=s)
+    d = Session()
+
+    result = d.query(State).filter(State.name.like('%a%')).order_by(State.id)\
+              .all()
+
+    for state in result:
+        print("{}: {}".format(state.id, state.name))
