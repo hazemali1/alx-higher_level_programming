22fadea954969d23bf745add2dd0b26f6af52df0
hazemali1 <hazemali1343@gmail.com>
2023-09-28 12:44:08 -0700

    peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""find peak"""
 
 
 def find_peak(list_of_integers):
+    """list of int"""
     if len(list_of_integers) == 0:
         return None
     if len(list_of_integers) == 1:
