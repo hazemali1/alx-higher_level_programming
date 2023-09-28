4357c0abcfef8ac7f067c5717958a330c9a1b297
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-09-28 22:07:51 +0300

    peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,14 +2,28 @@
 
 
 def find_peak(list_of_integers):
-	if len(list_of_integers) == 0:
-		return None
-	if len(list_of_integers) == 1:
-		return list_of_integers[0]
-	if list_of_integers[0] > list_of_integers[1]:
-		return list_of_integers[0]
-	if list_of_integers[-1] > list_of_integers[-2]:
-		return list_of_integers[-1]
-	for s in range(1, len(list_of_integers) - 1):
-		if list_of_integers[s] >= list_of_integers[s-1] and list_of_integers[s] >= list_of_integers[s+1]:
-			return list_of_integers[s]
+
+    if list_of_integers is None or len(list_of_integers) == 0:
+        return None
+
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
+
+    mid_idx = int(len(list_of_integers) / 2)
+
+    if mid_idx != len(list_of_integers) - 1:
+        if list_of_integers[mid_idx - 1] < list_of_integers[mid_idx] and\
+           list_of_integers[mid_idx + 1] < list_of_integers[mid_idx]:
+            return list_of_integers[mid_idx]
+    else:
+        if list_of_integers[mid_idx - 1] < list_of_integers[mid_idx]:
+            return list_of_integers[mid_idx]
+        else:
+            return list_of_integers[mid_idx - 1]
+
+    if list_of_integers[mid_idx - 1] > list_of_integers[mid_idx]:
+        a_list = list_of_integers[0:mid_idx]
+    else:
+        a_list = list_of_integers[mid_idx + 1:]
+
+    return find_peak(a_list)
