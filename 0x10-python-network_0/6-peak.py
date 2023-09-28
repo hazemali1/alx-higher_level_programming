4a1ab6a37f14f6f56fb9b3227f74155811f2c704
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-09-28 21:52:06 +0300

    peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def find_peak(list_of_integers):
+	if len(list_of_integers) == 0:
+		return None
+	if len(list_of_integers) == 1:
+		return list_of_integers[0]
+	if list_of_integers[0] > list_of_integers[1]:
+		return list_of_integers[0]
+	if list_of_integers[-1] > list_of_integers[-2]:
+		return list_of_integers[-1]
+	for s in range(1, len(list_of_integers) - 1):
+		if list_of_integers[s] > list_of_integers[s-1] and list_of_integers[i] > list_of_integers[i+1]:
+			return list_of_integers[s]
