0ba9852008a558b7a214e16cfcb39c5b081077d4
hazemali1 <hazemali1343@gmail.com>
2023-09-28 15:27:34 -0700

    you got me!

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # you got me!
-curl -sX PUT -L -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
+curl -s -L -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
