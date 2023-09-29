3cd77d110b7a2c532947bb51d46a8494d74b4d2a
hazemali1 <hazemali1343@gmail.com>
2023-09-29 13:19:21 -0700

    search

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+search
+"""
+
+
+if __name__ == "__main__":
+    """
+    main
+    """
+    import requests
+    import sys
+    """
+    import
+    """
+    s = ""
+    if len(sys.argv) > 1:
+        s = sys.argv[1]
+    q = {'q': s}
+    s = requests.post('http://0.0.0.0:5000/search_user', data=q)
+    if s.json() is None:
+        print('Not a valid JSON')
+    else:
+        if len(s.json()) == 0:
+            print('No result')
+        else:
+            print('[{}] {}'.format(s.json()['id'], s.json()['name']))
