88f2026ebf8d7a939bc43fd66a063189e3f0ad8d
hazemali1 <hazemali1343@gmail.com>
2023-09-29 16:14:29 -0700

    commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+github
+"""
+
+
+if __name__ == "__main__":
+    """
+    main
+    """
+    import requests
+    import sys
+    """
+    import
+    """
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(sys.argv[1],
+                                                              sys.argv[2])
+    s = requests.get(url)
+    count = 1
+    for d in s.json():
+        if count <= 10:
+            count += 1
+            print(d['sha'], ': ', d['commit']['author']['name'])
