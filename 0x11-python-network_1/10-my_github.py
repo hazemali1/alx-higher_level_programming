1d53900992096905e874655f6c77d285da3f654a
hazemali1 <hazemali1343@gmail.com>
2023-09-29 14:57:47 -0700

    github

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+github
+"""
+
+
+if __name__ == "__main__":
+    """
+    main
+    """
+    import requests
+    import sys
+    """
+    import
+    """
+    s = requests.get('https://api.github.com/user', auth=(sys.argv[1],
+                                                          sys.argv[2]))
+    try:
+        print(s.json()['id'])
+    except KeyError:
+        print(None)
