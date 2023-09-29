a5a8606226e205dcf6204ca2b05ec28a6c2ef18e
hazemali1 <hazemali1343@gmail.com>
2023-09-29 10:15:23 -0700

    x-id

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
-import urllib.request
-import sys
 """
 id
 """
 
+
 if __name__ == "__main__":
+    """
+    main
+    """
+    import urllib.request
+    import sys
+    """
+    import module
+    """
     with urllib.request.urlopen(sys.argv[1]) as s:
         d = s.headers.__dict__
         print(d['_headers'][-3][1])
