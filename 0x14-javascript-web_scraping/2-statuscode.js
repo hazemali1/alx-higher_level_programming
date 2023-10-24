24222088657bcbbe2a041ddb7079ba8e0360be60
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-10-24 21:58:34 +0300

    code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
 const request = require('request');
-request(process.argv[2], function (error, response) {
+request(process.argv[2], function (_error, response) {
   console.log('code:', response.statusCode);
 });
