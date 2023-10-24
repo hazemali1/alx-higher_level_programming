0f284d9ff6e7bf06d48a11718539052a47c6f697
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-10-24 23:27:36 +0300

    characters

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,15 +2,15 @@
 
 const request = require('request');
 let count = 0;
-id = "https://swapi-api.alx-tools.com/api/people/18/"
+const id = 'https://swapi-api.alx-tools.com/api/people/18/';
 request(process.argv[2], function (_error, _response, body) {
-	b = JSON.parse(body).results;
-	for (let i = 0; i < b.lenght; i++) {
-		for (let j = 0; j < b[i].characters.lenght; j++) {
-			if (body[i].characters[j] === id) {
-				count++;
-			}
-		}
-	}
+  const b = JSON.parse(body).results;
+  for (let i = 0; i < b.length; i++) {
+    for (let j = 0; j < b[i].characters.length; j++) {
+      if (b[i].characters[j] === id) {
+        count++;
+      }
+    }
+  }
   console.log(count);
 });
