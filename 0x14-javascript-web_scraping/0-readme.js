b1cfa233185d0e2cc7029f9ae790f8d9c3f04614
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-10-24 19:45:50 +0300

    readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,10 +3,9 @@
 const process = require('process');
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf-8', (err, data) => {
-	if (err) {
-		console.error(err);
-	}
-	else {
-		console.log(data);
-	}
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
 });
