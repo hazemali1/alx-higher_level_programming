d2ab1d59e803c71f83fa0f81136c9b4d89c323d2
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-10-25 00:22:07 +0300

    com

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,25 +1,25 @@
 #!/usr/bin/node
 
 const request = require('request');
-request(process.argv[2], function (error, _response, body) {
-	if (error) {
-    console.log(error);
+
+request(process.argv[2], function (err, _res, body) {
+  if (err) {
+    console.log(err);
   } else {
-		const myDict = {};
-		const b = JSON.parse(body);
-		let count = 0;
-		let num = 1;
-		for (let i = 0; i < b.length; i++) {
-			if (b[i].userId !== num) {
-				myDict[num] = count;
-				count = 0;
-				num = b[i].userId;
-			}
-			if (b[i].completed) {
-				count++;
-			}
-		}
-		myDict[num] = count;
-		console.log(myDict);
-	}
+    const completedTasksByUsers = {};
+    body = JSON.parse(body);
+
+    for (let i = 0; i < body.length; ++i) {
+      const userId = body[i].userId;
+      const completed = body[i].completed;
+
+      if (completed && !completedTasksByUsers[userId]) {
+        completedTasksByUsers[userId] = 0;
+      }
+
+      if (completed) ++completedTasksByUsers[userId];
+    }
+
+    console.log(completedTasksByUsers);
+  }
 });
