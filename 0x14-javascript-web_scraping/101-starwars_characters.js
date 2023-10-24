8b8d15438443a04c3d0711ebffa73c8e44f6e3ac
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-10-25 01:35:18 +0300

    star

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,10 +2,15 @@
 
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request(url, function (_error, _response, body) {
-  for (let i = 0; i < JSON.parse(body).characters.length; i++) {
-    request(JSON.parse(body).characters[i], function (_error2, _response2, body2) {
-      console.log(JSON.parse(body2).name);
-    });
+function print (url, index) {
+  if (index === url.length) {
+    return;
   }
+  request(url[index], function (_error2, _response2, body2) {
+    console.log(JSON.parse(body2).name);
+    print(url, index + 1);
+  });
+}
+request(url, function (_error, _response, body) {
+  print(JSON.parse(body).characters, 0);
 });
