57dc8e7aa9245bef954ad33888b90ffe835a197e
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-10-24 23:34:50 +0300

    characters

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,24 +1,16 @@
 #!/usr/bin/node
 
 const request = require('request');
-const starWarsUri = process.argv[2];
-let times = 0;
-
-request(starWarsUri, function (_err, _res, body) {
-  body = JSON.parse(body).results;
-
-  for (let i = 0; i < body.length; ++i) {
-    const characters = body[i].characters;
-
-    for (let j = 0; j < characters.length; ++j) {
-      const character = characters[j];
-      const characterId = character.split('/')[5];
-
-      if (characterId === '18') {
-        times += 1;
+let count = 0;
+request(process.argv[2], function (_error, _response, body) {
+  const b = JSON.parse(body).results;
+  for (let i = 0; i < b.length; i++) {
+    for (let j = 0; j < b[i].characters.length; j++) {
+			const char = b[i].characters[j].split('/')[5];
+      if (char === '18') {
+        count++;
       }
     }
   }
-
-  console.log(times);
+  console.log(count);
 });
