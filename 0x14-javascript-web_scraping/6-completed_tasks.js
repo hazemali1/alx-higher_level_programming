227d98f7e5f62a925733139c568356256fda0d24
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-10-25 00:38:17 +0300

    com

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -6,10 +6,6 @@ request(process.argv[2], function (_error, _response, body) {
   const b = JSON.parse(body);
   let num = 1;
   for (let i = 0; i < b.length; i++) {
-    if (b[i].userId !== num) {
-      myDict[b[i].userId] = 0;
-      num = b[i].userId;
-    }
     if (b[i].completed && !myDict[b[i].userId]) {
       myDict[b[i].userId] = 0;
     }
